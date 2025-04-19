5ec9ba9dba6e757e515e698a910f8e67e9e7185d
alexsvorada <alex@svorada.eu>
2025-04-19 08:39:11 +0200

    add support for 0.21.4, fix Slimefun api breaking changes

--- src/main/java/io/github/thebusybiscuit/extragear/ExtraGear.java
+++ src/main/java/io/github/thebusybiscuit/extragear/ExtraGear.java
@@ -38,49 +38,49 @@ public class ExtraGear extends JavaPlugin implements SlimefunAddon {
 
         new Metrics(this, 6469);
 
-        itemGroup = new ItemGroup(new NamespacedKey(this, "items"), new CustomItemStack(Material.DIAMOND_SWORD, "&6ExtraGear"), 1);
+        itemGroup = new ItemGroup(new NamespacedKey(this, "items"), CustomItemStack.create(Material.DIAMOND_SWORD, "&6ExtraGear"), 1);
 
-        registerSword(Material.IRON_SWORD, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 2)));
-        registerArmor(ArmorSet.LEATHER, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2)));
+        registerSword(Material.IRON_SWORD, "COPPER", SlimefunItems.COPPER_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SMITE, 2)));
+        registerArmor(ArmorSet.LEATHER, "COPPER", SlimefunItems.COPPER_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BLAST_PROTECTION, 2)));
 
-        registerSword(Material.IRON_SWORD, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 1)));
-        registerArmor(ArmorSet.IRON, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 3)));
+        registerSword(Material.IRON_SWORD, "TIN", SlimefunItems.TIN_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 1)));
+        registerArmor(ArmorSet.IRON, "TIN", SlimefunItems.TIN_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BLAST_PROTECTION, 3)));
 
-        registerSword(Material.IRON_SWORD, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2)));
-        registerArmor(ArmorSet.IRON, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2)));
+        registerSword(Material.IRON_SWORD, "SILVER", SlimefunItems.SILVER_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 2)));
+        registerArmor(ArmorSet.IRON, "SILVER", SlimefunItems.SILVER_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 2)));
 
-        registerSword(Material.IRON_SWORD, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 3)));
-        registerArmor(ArmorSet.IRON, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2), new Pair<>(Enchantment.DURABILITY, 2)));
+        registerSword(Material.IRON_SWORD, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BANE_OF_ARTHROPODS, 3)));
+        registerArmor(ArmorSet.IRON, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BLAST_PROTECTION, 2), new Pair<>(Enchantment.UNBREAKING, 2)));
 
-        registerSword(Material.IRON_SWORD, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 8)));
-        registerArmor(ArmorSet.IRON, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3), new Pair<>(Enchantment.DURABILITY, 8)));
+        registerSword(Material.IRON_SWORD, "LEAD", SlimefunItems.LEAD_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 3), new Pair<>(Enchantment.UNBREAKING, 8)));
+        registerArmor(ArmorSet.IRON, "LEAD", SlimefunItems.LEAD_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 3), new Pair<>(Enchantment.UNBREAKING, 8)));
 
-        registerSword(Material.IRON_SWORD, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2)));
-        registerArmor(ArmorSet.IRON, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3)));
+        registerSword(Material.IRON_SWORD, "ZINC", SlimefunItems.ZINC_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 2)));
+        registerArmor(ArmorSet.IRON, "ZINC", SlimefunItems.ZINC_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 3)));
 
-        registerSword(Material.IRON_SWORD, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2), new Pair<>(Enchantment.DURABILITY, 5)));
-        registerArmor(ArmorSet.IRON, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerSword(Material.IRON_SWORD, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 2), new Pair<>(Enchantment.UNBREAKING, 5)));
+        registerArmor(ArmorSet.IRON, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 2), new Pair<>(Enchantment.UNBREAKING, 5)));
 
-        registerSword(Material.IRON_SWORD, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 5), new Pair<>(Enchantment.DURABILITY, 6)));
-        registerArmor(ArmorSet.IRON, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3), new Pair<>(Enchantment.DURABILITY, 4)));
+        registerSword(Material.IRON_SWORD, "STEEL", SlimefunItems.STEEL_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 5), new Pair<>(Enchantment.UNBREAKING, 6)));
+        registerArmor(ArmorSet.IRON, "STEEL", SlimefunItems.STEEL_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 3), new Pair<>(Enchantment.UNBREAKING, 4)));
 
-        registerSword(Material.IRON_SWORD, "BRONZE", SlimefunItems.BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 6)));
-        registerSword(Material.IRON_SWORD, "DURALUMIN", SlimefunItems.DURALUMIN_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 6)));
-        registerSword(Material.IRON_SWORD, "BILLON", SlimefunItems.BILLON_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 4), new Pair<>(Enchantment.DURABILITY, 5)));
-        registerSword(Material.IRON_SWORD, "BRASS", SlimefunItems.BRASS_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 4), new Pair<>(Enchantment.DURABILITY, 6)));
-        registerSword(Material.IRON_SWORD, "ALUMINUM_BRASS", SlimefunItems.ALUMINUM_BRASS_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4), new Pair<>(Enchantment.DURABILITY, 4)));
-        registerSword(Material.IRON_SWORD, "ALUMINUM_BRONZE", SlimefunItems.ALUMINUM_BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4), new Pair<>(Enchantment.DURABILITY, 5)));
-        registerSword(Material.IRON_SWORD, "CORINTHIAN_BRONZE", SlimefunItems.CORINTHIAN_BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 5), new Pair<>(Enchantment.DURABILITY, 5)));
-        registerSword(Material.IRON_SWORD, "SOLDER", SlimefunItems.SOLDER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 4), new Pair<>(Enchantment.DURABILITY, 6)));
-        registerSword(Material.IRON_SWORD, "DAMASCUS_STEEL", SlimefunItems.DAMASCUS_STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 6), new Pair<>(Enchantment.DURABILITY, 7)));
-        registerSword(Material.IRON_SWORD, "HARDENED", SlimefunItems.HARDENED_METAL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 7), new Pair<>(Enchantment.DURABILITY, 10)));
-        registerSword(Material.IRON_SWORD, "REINFORCED", SlimefunItems.REINFORCED_ALLOY_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 8), new Pair<>(Enchantment.DURABILITY, 8)));
-        registerSword(Material.IRON_SWORD, "FERROSILICON", SlimefunItems.FERROSILICON, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 8), new Pair<>(Enchantment.DURABILITY, 4)));
-        registerSword(Material.GOLDEN_SWORD, "GILDED_IRON", SlimefunItems.GILDED_IRON, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 8), new Pair<>(Enchantment.DURABILITY, 10)));
-        registerSword(Material.IRON_SWORD, "NICKEL", SlimefunItems.NICKEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 6), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerSword(Material.IRON_SWORD, "BRONZE", SlimefunItems.BRONZE_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 3), new Pair<>(Enchantment.UNBREAKING, 6)));
+        registerSword(Material.IRON_SWORD, "DURALUMIN", SlimefunItems.DURALUMIN_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 3), new Pair<>(Enchantment.UNBREAKING, 6)));
+        registerSword(Material.IRON_SWORD, "BILLON", SlimefunItems.BILLON_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 4), new Pair<>(Enchantment.UNBREAKING, 5)));
+        registerSword(Material.IRON_SWORD, "BRASS", SlimefunItems.BRASS_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SMITE, 4), new Pair<>(Enchantment.UNBREAKING, 6)));
+        registerSword(Material.IRON_SWORD, "ALUMINUM_BRASS", SlimefunItems.ALUMINUM_BRASS_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BANE_OF_ARTHROPODS, 4), new Pair<>(Enchantment.UNBREAKING, 4)));
+        registerSword(Material.IRON_SWORD, "ALUMINUM_BRONZE", SlimefunItems.ALUMINUM_BRONZE_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.BANE_OF_ARTHROPODS, 4), new Pair<>(Enchantment.UNBREAKING, 5)));
+        registerSword(Material.IRON_SWORD, "CORINTHIAN_BRONZE", SlimefunItems.CORINTHIAN_BRONZE_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 5), new Pair<>(Enchantment.UNBREAKING, 5)));
+        registerSword(Material.IRON_SWORD, "SOLDER", SlimefunItems.SOLDER_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 4), new Pair<>(Enchantment.UNBREAKING, 6)));
+        registerSword(Material.IRON_SWORD, "DAMASCUS_STEEL", SlimefunItems.DAMASCUS_STEEL_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 6), new Pair<>(Enchantment.UNBREAKING, 7)));
+        registerSword(Material.IRON_SWORD, "HARDENED", SlimefunItems.HARDENED_METAL_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 7), new Pair<>(Enchantment.UNBREAKING, 10)));
+        registerSword(Material.IRON_SWORD, "REINFORCED", SlimefunItems.REINFORCED_ALLOY_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 8), new Pair<>(Enchantment.UNBREAKING, 8)));
+        registerSword(Material.IRON_SWORD, "FERROSILICON", SlimefunItems.FERROSILICON.item(), Arrays.asList(new Pair<>(Enchantment.SMITE, 8), new Pair<>(Enchantment.UNBREAKING, 4)));
+        registerSword(Material.GOLDEN_SWORD, "GILDED_IRON", SlimefunItems.GILDED_IRON.item(), Arrays.asList(new Pair<>(Enchantment.BANE_OF_ARTHROPODS, 8), new Pair<>(Enchantment.UNBREAKING, 10)));
+        registerSword(Material.IRON_SWORD, "NICKEL", SlimefunItems.NICKEL_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 6), new Pair<>(Enchantment.UNBREAKING, 5)));
 
-        registerSword(Material.IRON_SWORD, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 7), new Pair<>(Enchantment.DURABILITY, 7)));
-        registerArmor(ArmorSet.IRON, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 7), new Pair<>(Enchantment.DURABILITY, 7)));
+        registerSword(Material.IRON_SWORD, "COBALT", SlimefunItems.COBALT_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.SHARPNESS, 7), new Pair<>(Enchantment.UNBREAKING, 7)));
+        registerArmor(ArmorSet.IRON, "COBALT", SlimefunItems.COBALT_INGOT.item(), Arrays.asList(new Pair<>(Enchantment.PROTECTION, 7), new Pair<>(Enchantment.UNBREAKING, 7)));
     }
 
     private void registerSword(Material type, String component, ItemStack item, List<Pair<Enchantment, Integer>> enchantments) {
@@ -108,7 +108,7 @@ public class ExtraGear extends JavaPlugin implements SlimefunAddon {
                 new SlimefunItemStack(component + "_BOOTS", armorset.getBoots(), "&f" + humanizedComponent + " Boots") };
 
         for (Pair<Enchantment, Integer> enchantment : enchantments) {
-            for (ItemStack is : armor) {
+            for (SlimefunItemStack is : armor) {
                 is.addUnsafeEnchantment(enchantment.getFirstValue(), enchantment.getSecondValue());
             }
         }
